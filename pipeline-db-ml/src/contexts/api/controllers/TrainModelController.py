e44b22815dace9edce2d56c46658562f3ce394dc
0242324-cpu <0242324@up.edu.mx>
2026-04-08 19:37:28 -0600

    Add files via upload
    Archivos de la api

---
+++ pipeline-db-ml/src/contexts/api/controllers/TrainModelController.py
@@ -0,0 +1,32 @@
+import os
+import joblib
+import numpy as np
+import os
+
+from src.contexts.api.models import PredictorRequest
+
+
+
+class TrainModelController:
+    def execute(self, request: PredictorRequest):
+        print(request)
+        email=request.email
+        country = request.country
+        city= request.city
+       
+        # Cargar el modelo desde el archivo
+        modelo_cargado = joblib.load(os.getenv("MODELO_ENTRENADO"))
+        encoder= joblib.load(os.getenv("ENCODER_ENTRENADO"))
+        label_encoder = joblib.load(os.getenv("LABEL_ENCODER_ENTRENADO"))
+
+
+        # Creando y codificando el dato
+        nuevo_dato = encoder.transform(np.array([[email, country, city]]))
+
+        # Mandando el dato para hacer la predicción
+        result = modelo_cargado.predict(nuevo_dato)
+        prediction= label_encoder.inverse_transform(result)
+        print(f"Predicción: {prediction[0]}")
+        
+        return {"status": "OK", "result": prediction[0]}
+        
---
+++ pipeline-db-ml/src/contexts/api/models/PredictorRequest.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from typing import Literal
+
+EmailProvider = Literal[
+    "yahoo", "sapo", "comcast", "jetbrains", "woodstock",
+    "surfeu", "jubii", "embraer", "google", "riotur",
+    "hotmail", "gmail", "aol", "rediff", "wp",
+    "rogers", "shaw", "microsoft", "yachoo", "apple", "uol",
+]
+
+CountryProvider = Literal[
+    "Argentina", "Spain", "Italy", "Hungary", "India", "Czech Republic",
+    "Belgium", "Sweden", "Chile", "Norway", "France", "USA", "United Kingdom",
+    "Netherlands", "Brazil", "Austria", "Poland", "Australia", "Ireland",
+    "Germany", "Denmark", "Canada", "Finland", "Portugal"
+]
+
+CityProvider = Literal[
+    "Porto", "Budapest", "Reno", "London", "Paris", "New York",
+    "Salt Lake City", "Prague", "Dijon", "Rome", "Berlin", "Lyon",
+    "Bangalore", "Ottawa", "Orlando", "Fort Worth", "Chicago", "Mountain View",
+    "Stuttgart", "Delhi", "Stockholm", "Vienne", "Edinburgh",
+    "Halifax", "São Paulo", "Madison", "Amsterdam", "Montréal",
+    "Sidney", "Madrid", "Lisbon", "Warsaw", "Edmonton", "Vancouver",
+    "Dublin", "Toronto", "Santiago", "Brasília", "Bordeaux",
+    "Cupertino", "Brussels", "Redmond", "Buenos Aires",
+    "São José dos Campos", "Yellowknife", "Boston", "Tucson",
+    "Helsinki", "Winnipeg", "Copenhagen", "Oslo", "Frankfurt",
+    "Rio de Janeiro"
+]
+    
+
+
+class PredictorRequest(BaseModel):
+  email: EmailProvider
+  country: CountryProvider
+  city: CityProvider
